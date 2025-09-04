ca969dc8ffbad5b7da2ca62d290e5eb27c8b96dd
Guilelmy <guilhermy.aquino@estudante.iftm.edu.br>
2025-09-04 15:17:09 -0300

    feats: agendamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import { SearchIcon } from "lucide-react"
+import { Badge, SearchIcon } from "lucide-react"
 import Header from "./components/header"
 import { Button } from "./components/ui/button"
 import { Input } from "./components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./components/ui/card"
+import { Avatar, AvatarImage } from "./components/ui/avatar"
 
 export default function Home() {
   return (
@@ -26,6 +28,29 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3>Corte de cabelo</h3>
+              <div className="flex items-center">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage
+                    src="https://utfs.io/f/178da6b6-6f9a-424a-be9d-a2feb476eb36-16t.png"
+                    alt="avatar"
+                  />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Setembro</p>
+              <p className="text-2xl">04</p>
+              <p>20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
