c62371d56fce7b845a00e17cdd1693f4b13f1d3b
Guilelmy <guilhermy.aquino@estudante.iftm.edu.br>
2025-09-06 00:07:18 -0300

    feat: page

---
+++ src/app/barbershops/[id]/page.tsx
@@ -0,0 +1,63 @@
+import { Button } from "@/app/components/ui/button"
+import { db } from "@/app/lib/prisma"
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+interface BarbershopPageProps {
+  params: { id: string }
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  const barbershop = await db.barbershop.findUnique({
+    where: { id: params.id },
+  })
+  return (
+    <div>
+      <div className="relative h-[250px] w-full">
+        <Image
+          src={barbershop?.imageUrl ?? "/fallback-image.jpg"}
+          alt={barbershop?.name ?? "Barbershop"}
+          fill
+          className="object-cover"
+        />
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute left-4 top-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute right-4 top-4"
+        >
+          <MenuIcon></MenuIcon>
+        </Button>
+      </div>
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
+        <div className="mb-2 flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address}</p>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <StarIcon className="fill-primary text-primary" size={18} />
+          <p className="text-sm">TODO AVALIACAO</p>
+        </div>
+      </div>
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Sobre Nos</h2>
+        <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
