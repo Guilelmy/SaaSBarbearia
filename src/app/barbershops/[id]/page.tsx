82a3a35ef00be6564835030f149b1be47298c87a
Guilelmy <guilhermy.aquino@estudante.iftm.edu.br>
2025-09-06 09:15:49 -0300

    feat: servicos

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,8 +1,10 @@
+import ServiceItem from "@/app/components/service-item"
 import { Button } from "@/app/components/ui/button"
 import { db } from "@/app/lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 interface BarbershopPageProps {
   params: { id: string }
@@ -11,7 +13,13 @@ interface BarbershopPageProps {
 const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const barbershop = await db.barbershop.findUnique({
     where: { id: params.id },
+    include: { services: true },
   })
+
+  if (!barbershop) {
+    return notFound()
+  }
+
   return (
     <div>
       <div className="relative h-[250px] w-full">
@@ -49,12 +57,22 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
 
         <div className="flex items-center gap-1">
           <StarIcon className="fill-primary text-primary" size={18} />
-          <p className="text-sm">TODO AVALIACAO</p>
+          <p className="text-sm">TO DO AVALIACAO</p>
         </div>
       </div>
       <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre Nos</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+      <div className="p-5">
+        <h2 className="mb-3 text-xs font-bold uppercase text-gray-400">
+          Servicos
+        </h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
